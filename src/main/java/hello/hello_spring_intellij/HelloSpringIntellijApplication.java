4ca2ca6cede530a6a5b86972eefeb096c30638bb
april0391 <april0391@naver.com>
2024-06-12 15:46:58 +0900

    first commit

---
+++ src/main/java/hello/hello_spring_intellij/HelloSpringIntellijApplication.java
@@ -0,0 +1,13 @@
+package hello.hello_spring_intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringIntellijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hello_spring_intellij/HelloSpringIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_spring_intellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
