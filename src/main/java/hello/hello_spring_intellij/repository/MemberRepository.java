f2de661d271b20fe60e358751f2576f69ab188f8
april0391 <april0391@naver.com>
2024-06-12 18:40:26 +0900

    second

---
+++ src/main/java/hello/hello_spring_intellij/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring_intellij.repository;
+
+import hello.hello_spring_intellij.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/hello/hello_spring_intellij/service/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hello_spring_intellij.service;
+
+import hello.hello_spring_intellij.repository.MemberRepository;
+import hello.hello_spring_intellij.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
